3e11a56d26689c0211bf3e7243f390628da63d18
Lylay-09 <pozzlay549@gmail.com>
2024-05-24 13:14:54 +0700

    add dart.io lib testing

--- lib/chart/pie_chart.dart
+++ lib/chart/pie_chart.dart
@@ -1,6 +1,8 @@
 // import 'package:flutter/material.dart';
 // import 'package:fl_chart/fl_chart.dart';
 
+import 'dart:io';
+
 // class PieChartTest extends StatelessWidget {
 //   final List<PieChartSectionData> pieChartData = [
 //     PieChartSectionData(
@@ -52,36 +54,26 @@
 //   }
 // }
 
-
-
-
-
-
-
-
-
-
 // import 'package:pie_chart/pie_chart.dart';
 // import 'package:flutter/material.dart';
 // class PieChartTest extends StatelessWidget {
 //   PieChartTest({super.key});
 //   final dataMap = <String, double>{
 //     "Flutter": 15,
-   
+
 //   };
 
 //   final colorList = <Color>[
 //     Colors.greenAccent,
 //   ];
 
-//   @override 
+//   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
 //       appBar: AppBar(
 //         title: const Text("Create Pie Chart"),
 //         centerTitle: true,
-        
-        
+
 //       ),
 //       body: Container(
 //         padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -102,28 +94,17 @@
 //             showChartValuesInPercentage: true,
 //           ),
 //            totalValue:18, // the space over from data map in this int  over 4 value because flutter 16
-        
+
 //           legendOptions: const LegendOptions(
-            
+
 //             showLegends: false
 //           ),
-        
-          
+
 //         ),
 //       ),
 //     );
 //   }
 // }
-
-
-
-
-
-
-
-
-
-
 
 import 'package:pie_chart/pie_chart.dart';
 import 'package:flutter/material.dart';
@@ -166,7 +147,6 @@ class PieChartTest extends StatelessWidget {
           ringStrokeWidth: 32,
           centerText: "pie chart".toUpperCase(),
           legendOptions: const LegendOptions(
-            
             showLegendsInRow: false,
             legendPosition: LegendPosition.right,
             showLegends: true,
@@ -181,7 +161,6 @@ class PieChartTest extends StatelessWidget {
               showChartValuesInPercentage: true,
               showChartValuesOutside: false,
               decimalPlaces: 2,
-              
               chartValueStyle: TextStyle(fontSize: 14, color: Colors.black)),
           // gradientList: ---To add gradient colors---
           // emptyColorGradient: ---Empty Color gradient---
